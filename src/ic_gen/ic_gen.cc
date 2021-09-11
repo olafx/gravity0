fb3d900c60dbfa5c13fab9489390b3e354e6d0f0
Olaf Willocx <olaf@usa.com>
2021-09-11 22:20:02 +0200

    basic ic generation functionality separate from solver, and adapting solver to read the ic from file

---
+++ src/ic_gen/ic_gen.cc
@@ -0,0 +1,22 @@
+#include "ic_gen.hh"
+#include "../storage/storage.hh"
+
+int main()
+{   using namespace std;
+
+    size_t n {4};
+    
+    Storage storage {{"0.h5", H5F_ACC_EXCL}};
+
+    mt19937_64 generator;
+    normal_distribution<double> distribution {0, 10};
+
+    double *ic = new double[4*n];
+
+    for (size_t i {}; i < 4*n; i++)
+        ic[i] = distribution(generator);
+
+    storage.new_dataset(ic, 2*n, "ic");
+
+    delete[] ic;
+}
--- src/solvers/direct_verlet.cc
+++ src/solvers/direct_verlet.cc
@@ -3,46 +3,38 @@
 
 int main()
 {
-    // number of particles
-    size_t n {4};
-
     // time step
     double dt {0.00001};
-
     // # time steps, including ic
     size_t n_t {10000001};
-
     // store data every so many steps
     size_t n_s {1000};
 
 
+    // open h5 file with initial condition
+    Storage storage {{"0.h5", H5F_ACC_RDWR}};
 
-    // set up h5 storage
-    Storage storage {{"direct_verlet.h5", H5F_ACC_TRUNC}};
+    // get number of objects in initial condition
+    size_t n {storage.read_dataset_size("ic") / 2};
 
+    // memory allocation
     double *state = new double[4*n];
+    double *ic = new double[4*n];
 
-    // initial condition
-    double *ic = new double[4*n]
-    {   0.98, 0,
-        0, 1,
-        -1, 0,
-        0, -1,
+    // setting initial condition
+    storage.read_dataset(ic, "ic");
+    // storing initial condition pos as step 0
+    storage.new_dataset(ic, n, "0");
 
-        0, 1,
-        -1, 0,
-        0, -1,
-        1, 0
-    };
-
-    storage.new_dataset(ic, n, 0);
-
+    // initializing state to be ready for Verlet integration
     ic_init(ic, state, n, dt);
     delete[] ic;
 
+    // time loop
     for (size_t t {1}; t < n_t; t++)
-    {   if (t % n_s == 0)
-            storage.new_dataset(state, n, t/n_s);
+    {   // store state
+        if (t % n_s == 0)
+            storage.new_dataset(state, n, std::to_string(t/n_s));
         update(state, n, dt);
     }
 
