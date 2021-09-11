f514159df27d4bd97a7fc11abfe303b9d3538cda
Olaf Willocx <olaf@usa.com>
2021-09-11 21:53:20 +0200

    remove old size method and generalize integer names to string names

--- src/storage/storage.hh
+++ src/storage/storage.hh
@@ -8,42 +8,21 @@ struct Storage
 {
     H5File file;
 
-    void new_dataset(void *buffer, hsize_t size, uint64_t name)
+    void new_dataset(void *buffer, hsize_t size, std::string name)
     {   hsize_t shape[2] {size, 2};
         DataSpace dataspace {2, shape};
         FloatType datatype {PredType::NATIVE_DOUBLE};
-        DataSet dataset {file.createDataSet(std::to_string(name), datatype, dataspace)};
+        DataSet dataset {file.createDataSet(name, datatype, dataspace)};
         dataset.write(buffer, PredType::NATIVE_DOUBLE);
     }
 
-    /*
-    void new_size(uint64_t size)
-    {   DataSpace attr_dataspace {H5S_SCALAR};
-        IntType attr_datatype {PredType::NATIVE_UINT64};
-        Attribute attr {file.createAttribute("size", attr_datatype, attr_dataspace)};
-        attr.write(PredType::NATIVE_UINT64, &size);
-    }
-
-    void write_size(uint64_t size)
-    {   Attribute attr {file.openAttribute("size")};
-        attr.write(PredType::NATIVE_UINT64, &size);
-    }
-
-    uint64_t read_size()
-    {   Attribute attr {file.openAttribute("size")};
-        uint64_t buffer;
-        attr.read(PredType::NATIVE_UINT64, &buffer);
-        return buffer;
-    }
-    */
-
-    void read_dataset(void *buffer, uint64_t name)
-    {   DataSet dataset {file.openDataSet(std::to_string(name))};
+    void read_dataset(void *buffer, std::string name)
+    {   DataSet dataset {file.openDataSet(name)};
         dataset.read(buffer, PredType::NATIVE_DOUBLE);
     }
 
-    hsize_t read_dataset_size(uint64_t name)
-    {   DataSet dataset {file.openDataSet(std::to_string(name))};
+    hsize_t read_dataset_size(std::string name)
+    {   DataSet dataset {file.openDataSet(name)};
         DataSpace dataspace {dataset.getSpace()};
         hsize_t shape[2];
         dataspace.getSimpleExtentDims(shape);
