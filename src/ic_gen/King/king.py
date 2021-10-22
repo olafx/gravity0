767a2e41eb645ecaf5aad8b87821266df4c2a430
Olaf Willocx <olaf@usa.com>
2021-10-22 13:48:20 +0200

    comments, style improvements, data format fix for King sampling

--- src/ic_gen/King/king.py
+++ src/ic_gen/King/king.py
@@ -1,90 +1,161 @@
+'''
+King model cluster sampling following King (1966).
+
+Some effort is made to explain how King sampling works and how I get certain results where I deviate from King's path.
+
+The calculations don't follow the paper exactly, but the results (should be/are) identical.
+-   King evaluates the differential equation describing density as a function of potential numerically.
+    The differential equation indeed has no analytic solution, but the solution can be expressed using the error function, which
+    can at least be approximated analytically.
+-   King uses a boundary condition in the calculation of the partial differential equation describing density as a function
+    of radius. This boundary condition was determined based on experimentel data. I found that that this Gaussian differential
+    equation is extremely stable under this boundary condition; any remotely reasonable value gives indistinguishable results.
+    This makes me question the accuracy of said boundary condition. For this reason and the stability reason, I just pick 0
+    for said boundary condition.
+-   I don't bother undimensionalizing the final Gaussian differential equation.
+
+The sampling is done via uniform rejection sampling. Since the probability distribution function is numerically evaluated,
+rejection sampling is likely the most accurate and fastest sampling method for this problem (although uniform definitely isn't).
+'''
+
 import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 from scipy.special import erf
 import h5py
 
+#   Same variable names as King (1966).
 k = 1
 j = 1
 V0 = -1
 G = 1
+
+#   The number of radii to be linearly interpolated between when sampling.
+N = 100
+#   The number of stars to sample.
+n = 20
+
+#   In principle the previous variables define at what distance V hits 0, but the range of distances may be accidentally chosen
+#   too small in the numerical evaluation to reach that distance. The r_max variable is the maximum distance of said range.
+#   If the program exits with error 'could not continue since r0 > r_max', increase r_max here.
+#   Best not to make it too large since this will increase the distance between steps in the range. Make sure there are enough
+#   steps from the origin (which is printed during evaluation).
+#   Could in principle use estimates and/or trial and error to automate this and not need this variable.
 r_max = 1.5
-n = 100
-N = 20
+
+#   The name of the HDF5 file in which data will be stored.
 filename = '0.h5'
 
+#   The name of the dataset in which the data will be stored in the following format as a row major 2 * n by 3 doubles matrix:
+#       particle 1 pos x, particle 1 pos y, particle 1 pos z,
+#       ...,
+#       particle n pos x, particle n pos y, particle n pos z,
+#       particle 1 vel x, particle 1 vel y, particle 1 vel z,
+#       ...,
+#       particle n vel x, particle n vel y, particle n vel z
+dataset_name = 'ic'
+
+#   The density as a function of potential is the result of a non analytic differential equation.
+#   King solves it numerically. I solve it using the error function, which can be approximated analytically.
 def rho(V):
-    return 0 if V > 0 else np.pi**1.5*k/j**3*np.exp(2*j**2*(V0-V))*erf(j*np.sqrt(-2*V))\
-                         -2*np.pi*k*np.sqrt(-2*V)*np.exp(2*j**2*V0)*(1/(j**2)-4*V/3)
+    #   The piecewise nature of this function is not for numerical reasons, the density really is discontinuous due to galactic
+    #   tidal forces carrying away stars that wonder out beyond the cluster radius, which is the key idea behind the King model.
+    return 0 if V > 0 else np.sqrt(np.pi**3) * k / j**3\
+                           * np.exp(2 * j**2 * (V0 - V))\
+                           * erf(j * np.sqrt(-2 * V))\
+                           -2 * np.pi * k * np.sqrt(-2 * V)\
+                           * np.exp(2 * j**2 * V0)\
+                           * (1 / j**2 - 4 / 3 * V)
 
+#   This is the right hand side of the partial differential equation describing density as a function of radial distance.
+#   This differential equation is the differential version of Gauss's law for gravity in radial coordinates, under spherical
+#   symmetry.
 def rhs(r, q):
-    return [q[1], 4*np.pi*G*rho(q[0]) if r == 0 else 4*np.pi*G*rho(q[0])-2*q[1]/r]
+    a = 4 * np.pi * G * rho(q[0])
+    #   An inverse square field has a potential equal to zero at distance 0.
+    if r != 0:
+        a -= 2 * q[1] / r
+    return [q[1], a]
 
 
 
-r_eval = np.linspace(0, r_max, n)
-sol = solve_ivp(rhs, [0, r_max], [V0, 0], t_eval=r_eval)
+#   Notice the 0 initial condition mentioned earlier. King here would use some undimensionalized experimental value (8 I
+#   believe) in his undimensionalized version of the partial differential equation instead.
+sol = solve_ivp(rhs, [0, r_max], [V0, 0], t_eval = np.linspace(0, r_max, N))
 
 print('integration complete')
 
 
 
-V_interp = interp1d(sol.t, sol.y[0], assume_sorted=True)
+#   Plain linear interpolation. Anything else might make the boundary smooth, which is unphysical.
+V_interp = interp1d(sol.t, sol.y[0], assume_sorted = True)
 
+#   Check if the range was chosen sufficiently large.
 i = 0
-while sol.y[0,i] < 0:
+while sol.y[0, i] < 0:
     i += 1
-    if i == n:
+    if i == N:
         exit('could not continue since r0 > r_max')
-r0 = sol.t[i-1]-sol.y[0,i-1]*(r_eval[1]-r_eval[0])/(sol.y[0,i]-sol.y[0,i-1])
 
-print(f'cluster boundary is at {r0:.4f}, which is {n*r0/r_max:.2f} steps from the origin')
+#   Since the interpolation is linear, the exact boundary of the cluster can easily be evaluated.
+r0 = sol.t[i - 1] - sol.y[0, i - 1] * r_max / N / (sol.y[0, i] - sol.y[0, i - 1])
 
-samples_r = np.empty(N)
-samples_v = np.empty(N)
+print(f'cluster boundary is at {r0:.4f}, which is {N * r0 / r_max:.2f} steps from the origin')
+
+
+
+#   Rejection sampling the main probability distribution function.
+#   The radial distance sampling is simple. The velocity sampling range depends on the radial distance that was just sampled
+#   (because e.g. stars far from the cluster center with high gravitational energy must have low kinetic energy).
+def p(r, v):
+    return r**2 * v**2 * np.exp(-2 * j**2 * (V_interp(r) - V0))\
+           * (np.exp(-j**2 * v**2) - np.exp(j**2 * 2 * V_interp(r)))
+samples_r = np.empty(n)
+samples_v = np.empty(n)
 i = 0
 attempts = 0
-while i < N:
+while i < n:
     r = np.random.uniform(0, r0)
-    v = np.random.uniform(0, np.sqrt(-2*V_interp(r)))
-    p = np.random.uniform(0, 1/np.e**2)
-    if p < r**2*v**2*np.exp(-2*j**2*(V_interp(r)-V0))*(np.exp(-j**2*v**2)-np.exp(j**2*2*V_interp(r))):
+    v = np.random.uniform(0, np.sqrt(-2 * V_interp(r)))
+    p = np.random.uniform(0, 1 / np.e**2)
+    if p < p(r, v):
         samples_r[i] = r
         samples_v[i] = v
         i += 1
     attempts += 1
 
-print(f'r and v sampling completed with efficiency {100*i/attempts:.2f}%')
+print(f'r and v sampling completed with efficiency {i / attempts * 100:.2f}%')
 
 
 
-samples_r_polar = np.arccos(np.random.uniform(-1, 1, N))
-samples_v_polar = np.arccos(np.random.uniform(-1, 1, N))
-samples_r_azimuthal = np.random.uniform(0, 2*np.pi, N)
-samples_v_azimuthal = np.random.uniform(0, 2*np.pi, N)
+#   The sampling above did not include directional data. Position and velocity are isotropically distributed.
+samples_r_polar = np.arccos(np.random.uniform(-1, 1, n))
+samples_v_polar = np.arccos(np.random.uniform(-1, 1, n))
+samples_r_azimuthal = np.random.uniform(0, 2 * np.pi, n)
+samples_v_azimuthal = np.random.uniform(0, 2 * np.pi, n)
 
-print('r and v angular sampling complete')
+print('angular sampling complete')
 
 
 
-data = np.empty(6*N)
-
-for i in range(N):
-    data[6*i]   = samples_r[i]*np.sin(samples_r_polar[i])*np.cos(samples_r_azimuthal[i])
-    data[6*i+1] = samples_r[i]*np.sin(samples_r_polar[i])*np.sin(samples_r_azimuthal[i])
-    data[6*i+2] = samples_r[i]*np.cos(samples_r_polar[i])
-
-for i in range(N):
-    data[6*i+3] = samples_v[i]*np.sin(samples_v_polar[i])*np.cos(samples_v_azimuthal[i])
-    data[6*i+4] = samples_v[i]*np.sin(samples_v_polar[i])*np.sin(samples_v_azimuthal[i])
-    data[6*i+5] = samples_v[i]*np.cos(samples_v_polar[i])
+#   The samples are complete now, but should be transformed from spherical to Cartesian coordinates for storage and direct use
+#   by the solvers.
+data = np.empty(6 * n)
+for i in range(n):
+    data[3 * i]     = samples_r[i] * np.sin(samples_r_polar[i]) * np.cos(samples_r_azimuthal[i])
+    data[3 * i + 1] = samples_r[i] * np.sin(samples_r_polar[i]) * np.sin(samples_r_azimuthal[i])
+    data[3 * i + 2] = samples_r[i] * np.cos(samples_r_polar[i])
+for i in range(n):
+    data[3 * (i + n)]     = samples_v[i] * np.sin(samples_v_polar[i]) * np.cos(samples_v_azimuthal[i])
+    data[3 * (i + n) + 1] = samples_v[i] * np.sin(samples_v_polar[i]) * np.sin(samples_v_azimuthal[i])
+    data[3 * (i + n) + 2] = samples_v[i] * np.cos(samples_v_polar[i])
 
 print('spherical to Cartesian transformation complete')
 
 
 
 fp = h5py.File(filename, 'w')
-fp.create_dataset('ic', data=data, dtype=np.float64, shape=(2*N, 3))
+fp.create_dataset(dataset_path_and_name, data = data, dtype = np.float64, shape = (2 * n, 3))
 fp.close()
 
 print('data stored')
