5211ec808ecafd2b8a6dd3d422113147bfbb1849
Olaf Willocx <olaf@usa.com>
2021-10-22 14:18:41 +0200

    style

--- src/solvers/direct_verlet.cc
+++ src/solvers/direct_verlet.cc
@@ -3,38 +3,44 @@
 
 int main()
 {
-    // time step
-    double dt {0.00001};
-    // # time steps, including ic
-    size_t n_t {10000001};
-    // store data every so many steps
-    size_t n_s {1000};
+    //  time step
+    double dt = 0.00001;
+
+    //  number of time steps, including ic
+    size_t n_t = 10000001;
+
+    //  store data every so many steps
+    size_t n_s = 1000;
 
 
-    // open h5 file with initial condition
+
+    //  open h5 file with initial condition
     Storage storage {{"0.h5", H5F_ACC_RDWR}};
 
-    // get number of objects from initial condition
-    size_t n {storage.read_dataset_size("ic")/2};
+    //  get number of objects from initial condition
+    size_t n = storage.read_dataset_size("ic") / 2;
 
-    // memory allocation
-    double *state = new double[6*n];
-    double *ic = new double[6*n];
+    //  memory allocation
+    double *state = new double[6 * n],
+           *ic    = new double[6 * n];
 
-    // reading initial condition
+    //  reading initial condition
     storage.read_dataset(ic, "ic");
-    // storing initial condition pos as step 0
+
+    //  storing initial condition pos as step 0
     storage.new_dataset(ic, n, "0");
 
-    // initializing state to be ready for Verlet integration
+
+
+    //  initializing state to be ready for Verlet integration
     init(ic, state, n, dt);
     delete[] ic;
 
-    // time loop
-    for (size_t s {1}; s < n_t; s++)
-    {   // store state
+    //  time loop
+    for (size_t s = 1; s < n_t; s++)
+    {   //  store state
         if (s % n_s == 0)
-            storage.new_dataset(state, n, std::to_string(s/n_s));
+            storage.new_dataset(state, n, std::to_string(s / n_s));
         update(state, n, dt);
     }
 
--- src/solvers/direct_verlet.hh
+++ src/solvers/direct_verlet.hh
@@ -26,44 +26,44 @@ initialize state using ic of size n via 2nd order Taylor series with timestep dt
 void init(const double *const ic, double *const state, const size_t n, const double dt)
 {
     // state pos 2 prep for init
-    for (size_t i {}; i < 3*n; i++)
+    for (size_t i = 0; i < 3 * n; i++)
         state[i] = 0;
 
     // state pos 1 is given by ic
-    for (size_t i {}; i < n; i++)
-    {   state[3*(i+n)]   = ic[3*i];
-        state[3*(i+n)+1] = ic[3*i+1];
-        state[3*(i+n)+2] = ic[3*i+2];
+    for (size_t i = 0; i < n; i++)
+    {   state[3 * (i + n)]     = ic[3 * i];
+        state[3 * (i + n) + 1] = ic[3 * i + 1];
+        state[3 * (i + n) + 2] = ic[3 * i + 2];
     }
 
     // state pos 2 via 2nd order Taylor series
 
     // PP loop
-    for (size_t i {}; i < n; i++)
-    {   for (size_t j {}; j < n; j++)
+    for (size_t i = 0; i < n; i++)
+    {   for (size_t j = 0; j < n; j++)
         {   // 2nd Taylor term
             if (i != j)
             {   // pos 1 distance
-                double b1 {state[3*(i+n)]  -state[3*(j+n)]};
-                double b2 {state[3*(i+n)+1]-state[3*(j+n)+1]};
-                double b3 {state[3*(i+n)+2]-state[3*(j+n)+2]};
+                double b1 = state[3 * (i + n)]     - state[3 * (j + n)];
+                double b2 = state[3 * (i + n) + 1] - state[3 * (j + n) + 1];
+                double b3 = state[3 * (i + n) + 2] - state[3 * (j + n) + 2];
                 // pos 1 common acceleration factor
-                double c {1/sqrt(b1*b1+b2*b2+b3*b3)};
-                c = c*c*c;
+                double c = 1 / sqrt(b1 * b1 + b2 * b2 + b3 * b3);
+                c = c * c * c;
                 // add acceleration factor
-                state[3*i]   -= c*b1;
-                state[3*i+1] -= c*b2;
-                state[3*i+2] -= c*b3;
+                state[3 * i]     -= c * b1;
+                state[3 * i + 1] -= c * b2;
+                state[3 * i + 2] -= c * b3;
             }
         }
         // add velocity from ic, fix 1st and 2nd Taylor terms
-        state[3*i]   = (state[3*i]  *(0.5*dt)+ic[3*(i+n)])  *dt;
-        state[3*i+1] = (state[3*i+1]*(0.5*dt)+ic[3*(i+n)+1])*dt;
-        state[3*i+2] = (state[3*i+2]*(0.5*dt)+ic[3*(i+n)+2])*dt;
+        state[3 * i]     = (state[3 * i]     * (0.5 * dt) + ic[3 * (i + n)])     * dt;
+        state[3 * i + 1] = (state[3 * i + 1] * (0.5 * dt) + ic[3 * (i + n) + 1]) * dt;
+        state[3 * i + 2] = (state[3 * i + 2] * (0.5 * dt) + ic[3 * (i + n) + 2]) * dt;
         // 0th Taylor term
-        state[3*i]   += state[3*(i+n)];
-        state[3*i+1] += state[3*(i+n)+1];
-        state[3*i+2] += state[3*(i+n)+2];
+        state[3 * i]     += state[3 * (i + n)];
+        state[3 * i + 1] += state[3 * (i + n) + 1];
+        state[3 * i + 2] += state[3 * (i + n) + 2];
     }
 }
 
@@ -73,37 +73,37 @@ update state of size n via direct Verlet integration with timestep dt
 void update(double *const state, const size_t n, const double dt)
 {
     // PP loop
-    for (size_t i {}; i < n; i++)
+    for (size_t i = 0; i < n; i++)
     {   // acceleration counters
-        double a1 {};
-        double a2 {};
-        double a3 {};
-        for (size_t j {}; j < n; j++)
+        double a1 = 0;
+        double a2 = 0;
+        double a3 = 0;
+        for (size_t j = 0; j < n; j++)
         {   if (i != j)
             {   // pos 1 distance
-                double b1 {state[3*i]  -state[3*j]};
-                double b2 {state[3*i+1]-state[3*j+1]};
-                double b3 {state[3*i+2]-state[3*j+2]};
+                double b1 = state[3 * i]     - state[3 * j];
+                double b2 = state[3 * i + 1] - state[3 * j + 1];
+                double b3 = state[3 * i + 2] - state[3 * j + 2];
                 // pos 1 common acceleration factor
-                double c {1/sqrt(b1*b1+b2*b2+b3*b3)};
-                c = c*c*c;
+                double c = 1 / sqrt(b1 * b1 + b2 * b2 + b3 * b3);
+                c = c * c * c;
                 // add acceleration factor
-                a1 -= c*b1;
-                a2 -= c*b2;
-                a3 -= c*b3;
+                a1 -= c * b1;
+                a2 -= c * b2;
+                a3 -= c * b3;
             }
         }
         // save pos 1
-        double d1 {state[3*(i+n)]};
-        double d2 {state[3*(i+n)+1]};
-        double d3 {state[3*(i+n)+2]};
+        double d1 = state[3 * (i + n)];
+        double d2 = state[3 * (i + n) + 1];
+        double d3 = state[3 * (i + n) + 2];
         // set pos 1 to pos 2
-        state[3*(i+n)]   = state[3*i];
-        state[3*(i+n)+1] = state[3*i+1];
-        state[3*(i+n)+2] = state[3*i+2];
+        state[3 * (i + n)]     = state[3 * i];
+        state[3 * (i + n) + 1] = state[3 * i + 1];
+        state[3 * (i + n) + 2] = state[3 * i + 2];
         // new pos 2 via saved pos 1 and acceleration factor
-        state[3*i]   += state[3*i]  -d1+a1*(dt*dt);
-        state[3*i+1] += state[3*i+1]-d2+a2*(dt*dt);
-        state[3*i+2] += state[3*i+2]-d3+a3*(dt*dt);
+        state[3 * i]     += state[3 * i]     - d1 + a1 * (dt * dt);
+        state[3 * i + 1] += state[3 * i + 1] - d2 + a2 * (dt * dt);
+        state[3 * i + 2] += state[3 * i + 2] - d3 + a3 * (dt * dt);
     }
 }
