36d28d6f33e11ed714d2a7ab79739eebfc9f51c5
Olaf Willocx <olaf@usa.com>
2021-11-19 04:48:57 +0100

    clang tidy

--- src/solvers/direct_leapfrog.cc
+++ src/solvers/direct_leapfrog.cc
@@ -5,14 +5,13 @@
 
 int main()
 {
-
     /* number of time steps  */ std::size_t n_t = 1000000;
     /* steps between process */ std::size_t n_s = 1000;
     /* time step             */ double dt   = 1e-5;
     /* softening eps^2       */ double eps2 = 3e-5;
     /* h5 file with init con */ Storage storage {{"0.h5", H5F_ACC_RDWR}};
     /* number of objs        */ std::size_t n = storage.read_dataset_size("ic");
-    /* integrator memory     */ double *state = new double[6*n];
+    /* integrator memory     */ auto *state = new double[6*n];
 
     /* read and store ic     */ storage.read_dataset(state, "ic");
                                 storage.new_dataset(state, n, "0");
@@ -24,9 +23,9 @@ int main()
     {   storage.new_dataset(state, n, std::to_string(s / n_s));
         std::cout << s << '/' << n_t << '\n';
         //  TODO temp
-        Storage2 storage {"1.xdmf"};
-        storage.set(state, n);
-        storage.write();
+        Storage2 storage2 {"1.xdmf"};
+        storage2.set(state, n);
+        storage2.write();
         exit(0);
     };
 
--- src/solvers/direct_verlet.cc
+++ src/solvers/direct_verlet.cc
@@ -4,15 +4,14 @@
 
 int main()
 {
-
     /* number of time steps  */ std::size_t n_t = 1000000;
     /* steps between process */ std::size_t n_s = 1000;
     /* time step             */ double dt   = 1e-5;
     /* softening eps ^2      */ double eps2 = 3e-5;
     /* h5 file with init con */ Storage storage {{"0.h5", H5F_ACC_RDWR}};
     /* number of objs        */ std::size_t n = storage.read_dataset_size("ic");
-    /* integrator memory     */ double *ic    = new double[6*n];
-                                double *state = new double[6*n];
+    /* integrator memory     */ auto *ic    = new double[6*n];
+                                auto *state = new double[6*n];
 
     /* read and store ic     */ storage.read_dataset(ic, "ic");
                                 storage.new_dataset(ic, n, "0");
