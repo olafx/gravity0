0bafb5902fb7f834033ef629471070fbda26e040
Olaf Willocx <olaf@usa.com>
2021-11-19 14:38:53 +0100

    compiler optimizations and kahan sum implementation in prep

---
+++ src/solvers/kahan-babuska.hh
@@ -0,0 +1,17 @@
+#include <cstddef>
+
+namespace Kahan_Babuska
+{
+    double sum(const double *const data, const std::size_t n)
+    {
+        double sum = 0;
+        double compensator = 0;
+        for (std::size_t i = 0; i < n; i++)
+        {   double a = data[i] - c;
+            double b = sum + a;
+            compensator = (b - sum) - a;
+            sum = t;
+        }
+        return sum;
+    }
+}
